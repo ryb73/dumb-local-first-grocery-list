f13e4c7ac377e74b09812e75074ecb0596de7c2b
Ryan <rpbiwer@gmail.com>
2025-10-31 13:14:55 +0000

    CORS

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -43,7 +43,13 @@ const changeNotifier = new EventEmitter<ChangeNotifierEvents>();
 const LONG_POLL_TIMEOUT = 45_000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env[`ALLOWED_ORIGINS`]?.split(`,`) ?? [
+      `http://localhost:3000`,
+    ],
+  })
+);
 app.use(express.json({ limit: `10mb` }));
 
 // Request logging middleware
